989c0273c48f52eafa578c578dd53143131b17c3
unknown <joeffrey.chaucer.paitan@gmail.com>
2022-05-26 12:42:32 +0800

    added post command

--- .buildkite/steps/post-command.sh
+++ .buildkite/steps/post-command.sh
@@ -1,1 +1,7 @@
 echo "This is post-command script"
+
+echo BUILDKITE_COMMAND_EXIT_STATUS $BUILDKITE_COMMAND_EXIT_STATUS
+
+if [ $BUILDKITE_COMMAND_EXIT_STATUS -ne 0 ]; then
+    echo "Steps are skipped because $BUILDKITE_AGENT_NAME:$BUILDKITE_LABEL returned $BUILDKITE_COMMAND_EXIT_STATUS" > .buildkite/script.sh
+fi
