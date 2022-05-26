a68475687c2d7fffae65f03299c9ef7b7ef24721
unknown <joeffrey.chaucer.paitan@gmail.com>
2022-05-26 14:14:44 +0800

    added-script

--- .buildkite/hooks/pre-command.sh
+++ .buildkite/hooks/pre-command.sh
@@ -1,12 +1,28 @@
-echo FIRST_STEP: YES
+#!/bin/bash
+set -euo pipefail
 
-if [[ -n "$FIRST_STEP" ]]; then
-    rm -f .buildkite/script.sh || true
+echo $BUILDKITE_PULL_REQUEST_BASE_BRANCH
+echo $BUILDKITE_COMMIT
+
+
+git diff --stat $BUILDKITE_PULL_REQUEST_BASE_BRANCH...$BUILDKITE_COMMIT
+
+
+if ! git diff --name-only $BUILDKITE_PULL_REQUEST_BASE_BRANCH...$BUILDKITE_COMMIT | grep -qvE '(*.md)'
+then
+    echo "Only docs were updated, not running the CI."
+    SKIP_STEPS=$yes
     exit 0
 fi
 
-if [[ -f ".buildkite/script.sh" ]]; then
-    SKIP_STEPS=$(cat .buildkite/script.sh)
+
+if [[ -n "$FIRST_STEP" ]]; then
+    rm -f .buildkite/pipeline.yml || true
+    exit 0
+fi
+
+if [[ -f ".buildkite/pipeline.yml" ]]; then
+    SKIP_STEPS=$(cat .buildkite/pipeline.yml)
     export SKIP_STEPS
     echo SKIP_STEPS: $SKIP_STEPS
     exit 1
